3d0972b063351accadab5e4adb4491811c409934
kmsgm <minseo.kim@friendli.ai>
2023-10-26 01:34:49 +0900

    overwrite

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,5 @@
 from rest_framework.routers import SimpleRouter
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 from . import views
 
 urlpatterns = [
