d5fd9be55e27a519c12a45e62e81c8071501d3f7
kmsgm <minseo.kim@friendli.ai>
2023-10-26 01:20:53 +0900

    fix url

--- base/urls.py
+++ base/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("about/", views.about_info, name="about"),
     path("projects/", views.projects_list, name="projects"),
     path(
-        "projects/<str:category>",
+        "projects/<str:category>/",
         views.filtered_projects_list,
         name="filtered_projects",
     ),
