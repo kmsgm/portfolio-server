88becaf2146d65cf263a433b386a9016eea1712f
kmsgm <minseo.kim@friendli.ai>
2023-10-26 15:42:27 +0900

    cors error

--- backend/settings.py
+++ backend/settings.py
@@ -130,12 +130,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ORIGIN_ALLOW_ALL = True
-
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
+    "https://portfolio-web-kmsgm.vercel.app/",
+    "https://portfolio-web-khaki.vercel.app/",
+    "https://minseo-portfolio.shop",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://portfolio-web-kmsgm.vercel.app/",
+    "https://portfolio-web-khaki.vercel.app/",
+    "https://minseo-portfolio.shop",
 ]
